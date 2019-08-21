9c3acead1fa7ade9da9d76d23956d02a445b0753
Xanadu丶zxl <xanadu.zxl@gmail.com>
2019-08-21 23:45:04 +0800

    Update Ajax.js

--- Ajax.js
+++ Ajax.js
@@ -10,3 +10,20 @@ xhr.onreadystatechange = function () {
 
 xhr.open('GET', '/api', false)
 xhr.send(null)
+
+
+
+var xhr = new XMLHttpRequest() //声明一个请求对象
+
+
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4) { //readyState 4代表已向服务器发送请求
+    if (xhr.status === OK) { // status 200代表服务器返回是成功的
+      console.log(xhr.responseText); //这是返回的文本
+    } else {
+      console.log("Error: " + xhr.status);   //链接失败的时候抛出错误
+    }
+  }
+}
+
+xhr.open('GET', 'xxxx')
