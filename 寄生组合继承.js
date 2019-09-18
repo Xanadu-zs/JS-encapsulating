c0c64655a16625518dfefabb0a2281c28a4a2c56
Xanadu丶zxl <xanadu.zxl@gmail.com>
2019-09-18 23:14:54 +0800

    Update 寄生组合继承.js

--- 寄生组合继承.js
+++ 寄生组合继承.js
@@ -1,3 +1,26 @@
+// 组合继承2
+function Parent(value) {
+	this.val = value
+}
+Parent.prototype.getValue = function () {
+	cosnole.log(this.val)
+}
+
+function Child(value) {
+	Parent.call(this, value)
+}
+
+Child.prototype = Parent.prototype
+const child = new Child(1)
+
+
+child.getValue()
+child instanceof Parent
+
+
+
+
+// 最优版
 function Parent(value) {
 	this.val = value
 }
